196be094a075863f2ce536d940e7ee90557d9477
Mayer Bálint <center@gymoneglobal.com>
2024-09-09 19:54:08 +0200

    V1.0.0
    Site created (+)
    Placeholder page uploaded (+)
    ONLY ENGLISH

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="hu">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>GYM One - Demo</title>
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="shortcut icon" href="http://gymoneglobal.com/assets/img/logo.png" type="image/x-icon">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<link rel="shortcut icon" href="https://gymoneglobal.com/assets/img/logo.png" type="image/x-icon">
+
+<meta name="description"
+    content="GYM One - See you soon!">
+<meta name="keywords"
+    content="open-source, free gym software, GYM One, fitness studio management, client management, booking system, financial tracking, gym software, fitness club management, personal trainer software, sports club management, membership management, gym administration, digital gym, fitness app, gymnasium software, online booking, comprehensive fitness management, workout tracking, membership system">
+<meta name="author" content="GYMOne">
+<meta name="theme-color" content="#004de6">
+<link rel="icon" href="http://gymoneglobal.com/assets/img/logo.png">
+
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:site" content="@GYMOne">
+<meta name="twitter:creator" content="@GYMOne">
+<meta name="twitter:title" content="Demo">
+<meta name="twitter:description"
+    content="GYM One - See you soon!">
+<meta name="twitter:image" content="http://gymoneglobal.com/assets/img/logo.png">
+
+<meta property="og:title" content="Demo Page">
+<meta property="og:type" content="website">
+<meta property="og:url" content="http://gymoneglobal.com/">
+<meta property="og:site_name" content="GYM One">
+<meta property="og:description"
+    content="GYM One - See you soon!">
+<meta property="og:image" content="http://gymoneglobal.com/assets/img/logo.png">
+</head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-75NV275ZQS"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-75NV275ZQS');
+</script>
+
+<body>
+    <!-- NAVBAR START -->
+    <div class="container">
+        <div class="row text-center">
+            <div class="mt-5"></div>
+            <div class="col-md-8 mx-auto text-center mb-5">
+                <img class="img img-fluid" width="50%" src="http://gymoneglobal.com/assets/img/text-logo.png" alt="See you soon!">
+                <h1 class="mb-3 display-1">👋 See you soon! 👋</h1>
+                <p class="lead">This site <b>will be running</b> the latest version of GYM One so users can learn more about the benefits and features of the system before it goes live. Here we explain why you should choose GymOne and what benefits it offers users.</p>
+            </div>
+        </div>
+    </div>
+    <p class="small text-center mb-0">
+        Copyright © 2024 GYM One - All rights reserved. &nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-heart-fill" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314">
+            </path>
+        </svg>
+        - <a href="https://www.mayerbalint.hu/">Mayer Bálint</a>
+    </p>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
